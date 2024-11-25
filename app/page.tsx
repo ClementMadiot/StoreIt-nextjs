f75916396f067b6f029062f2c9e7b6c3a6c70c60
ClementMadiot <clementmadiot09@gmail.com>
2024-11-25 15:09:12 +0100

    globalcss

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-brand">StoreIt - The only solution you need.</h1>
+    <div className="center">
+      <h1 className="h1">StoreIt - The only solution you need.</h1>
     </div>
   );
 }
