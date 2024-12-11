9aeebfbcc7e0efe87dafe864710ab28ea3354b3b
ClementMadiot <clementmadiot09@gmail.com>
2024-12-11 14:17:02 +0100

    sorting

--- constants/index.ts
+++ constants/index.ts
@@ -9,32 +9,58 @@ export const navItems = [
 export const avatarplaceholderUrl =
   "https://img.freepik.com/psd-gratuit/illustration-3d-personne-lunettes-soleil_23-2149436188.jpg?semtu003dais_hybrid";
 
-  export const actionsDropdownItems = [
-    {
-      label: 'Rename',
-      icon: '/assets/icons/edit.svg',
-      value: 'rename',
-    },
-    {
-      label: 'Details',
-      icon: '/assets/icons/info.svg',
-      value: 'details',
-    },
-    {
-      label: 'Share',
-      icon: '/assets/icons/share.svg',
-      value: 'share',
-    },
-    {
-      label: 'Download',
-      icon: '/assets/icons/download.svg',
-      value: 'download',
-    },
-    {
-      label: 'Delete',
-      icon: '/assets/icons/delete.svg',
-      value: 'delete',
-    },
-  ];
+export const actionsDropdownItems = [
+  {
+    label: "Rename",
+    icon: "/assets/icons/edit.svg",
+    value: "rename",
+  },
+  {
+    label: "Details",
+    icon: "/assets/icons/info.svg",
+    value: "details",
+  },
+  {
+    label: "Share",
+    icon: "/assets/icons/share.svg",
+    value: "share",
+  },
+  {
+    label: "Download",
+    icon: "/assets/icons/download.svg",
+    value: "download",
+  },
+  {
+    label: "Delete",
+    icon: "/assets/icons/delete.svg",
+    value: "delete",
+  },
+];
 
+export const sortTypes = [
+  {
+    label: "Date created (newest)",
+    value: "$createdAt-desc",
+  },
+  {
+    label: "Created Date (oldest)",
+    value: "$createdAt-asc",
+  },
+  {
+    label: "Name (A-Z)",
+    value: "name-asc",
+  },
+  {
+    label: "Name (Z-A)",
+    value: "name-desc",
+  },
+  {
+    label: "Size (Highest)",
+    value: "size-desc",
+  },
+  {
+    label: "Size (Lowest)",
+    value: "size-asc",
+  },
+];
 export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
